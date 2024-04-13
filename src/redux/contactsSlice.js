37d20327c553873a7564ca3c74b38696871084dc
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2024-04-13 22:11:50 +0300

    next comit
    delite persist

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 import contactsData from "../contactsData.json"
 import { nanoid } from "nanoid";
+// import {createAsyncThunk} from "@reduxjs/toolkit"
+
+
+// const fetchGetContactsThunkAPI = createAsyncThunk("getContact/fetchGetContact", async (contactId, thunkAPI)=> {
+// try {
+//     const res = await axios.get("contactId")
+//     return res.data
+// } catch (error) {
+//     return thunkAPI.rejectWithValue(error.message)
+// }
+// })
 
 const contactsSlice = createSlice({
     name: "contacts",
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import persistStore from "redux-persist/es/persistStore";
+
 import { filtersReducer } from "./filtersSlice";
-import persistReducer from "redux-persist/es/persistReducer";
+
 import { contactsReducer } from "./contactsSlice";
-import storage from "redux-persist/lib/storage";
-
-
-const persistConfig = {
-    key: "contacts",
-    storage,
-    whitelist: ["items"],
-  };
-
-  const persistedReducer = persistReducer(persistConfig, contactsReducer)
 
 export const store = configureStore({
     reducer: {
-        contacts: persistedReducer,
+        contacts: contactsReducer,
         filters: filtersReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
 });
 
-export const persistor = persistStore(store)
