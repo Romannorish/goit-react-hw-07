f394c8ecb6f5c641e7b359259816ce1ca2f2cdb4
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2024-04-15 20:57:27 +0300

    next comit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,6 +3,7 @@ import {ErrorMessage, Field, Form, Formik} from "formik"
 import * as Yup from "yup"
 import {useDispatch} from "react-redux"
 import {addContacts} from "../../redux/contactsOps"
+
 export default function ContactForm() {
   const dispatch = useDispatch()
   const InitialData = {
